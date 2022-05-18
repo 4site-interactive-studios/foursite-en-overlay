6b7b1dc140269ecfe55bbdc823cba71871f904f7
Fernando Santos <fern@ndo.io>
2022-05-18 12:54:26 -0300

    A project is born 🐣

---
+++ README.md
@@ -0,0 +1,60 @@
+# Oceana Engaging Networks Overlay
+
+This project makes it easy to add an Overlay with a custom Engaging Networks Form to your website.
+
+## How to use
+
+1. Add the script below to the page:
+
+```html
+<script
+  defer="defer"
+  src="https://aaf1a18515da0e792f78-c27fdabe952dfc357fe25ebf5c8897ee.ssl.cf5.rackcdn.com/1997/oceana-en-overlay.js"
+  data-title="This is my title!"
+  data-subtitle="This is my subtitle"
+  data-paragraph="This is my paragraph"
+  data-image="https://source.unsplash.com/random/1920x1080"
+></script>
+```
+
+2. You can add any options as a data attribute on the `script` tag.
+
+## Options
+
+Every option should be set as a data attribute on the `script` tag. All options are optional, except for the `data-title`, `data-subtitle`, `data-paragraph`, and `data-image` options, which are empty by default.
+
+- **data-cookie_name**: The name of the cookie that will be used to store the closed state. Defaults to `hideOverlay`.
+- **data-cookie_expiry**: The number of days until the cookie expires. Default is `1`.
+- **data-title** - Title of the overlay.
+- **data-subtitle** - Subtitle of the overlay.
+- **data-paragraph** - Text of the overlay.
+- **data-image** - Background Image of the overlay.
+- **data-button_label** - Label of the donate button. Defaults to `Donate Now`.
+- **data-other_label** - Label of Other Amount field. Defaults to `$ other`. If empty, the field will be hidden.
+- **data-donation_form** - URL of the donation page.
+- **data-trigger** - How the user will trigger the overlay. Defaults to `0`, which means the overlay will automatically trigger when the page loads. Check the **Trigger Options** section for more information.
+
+## Trigger Options
+
+- **ANY_NUMBER** (example: `2`) - The overlay will open after NUMBER seconds.
+- **ANY_PIXEL** (example `400px`) - The overlay will open when the user scrolls the page for PIXEL pixels.
+- **ANY_PERCENT** (example `50%`) - The overlay will open when the user scrolls the page for PERCENT of the page.
+- **exit** (example `exit`) - The overlay will open when the user moves their mouse out of the page (exit intent).
+
+## Development
+
+Project's code is on the `src/app.ts` file. Styling changes must be on `src/sass` folder.
+
+## Install Dependencies
+
+1. `npm install`
+
+## Deploy
+
+1. `npm run build` - Builds the project
+2. `npm run start` - Watch for changes and rebuilds the project
+
+It's going to create a `dist` folder, where you can get the `oceana-en-overlay.js` file and publish it.
+
+Currently it's published on:  
+https://aaf1a18515da0e792f78-c27fdabe952dfc357fe25ebf5c8897ee.ssl.cf5.rackcdn.com/1997/oceana-en-overlay.js
---
+++ src/app.ts
@@ -0,0 +1,255 @@
+import { crumbs } from "./crumbs";
+
+export class App {
+  private overlayID = "oceana-" + Math.random().toString(36).substring(7);
+  private amounts = [35, 75, 100, 250, 500];
+  private overlay: HTMLElement;
+  private triggered = false;
+  private options: { [key: string]: string } = {
+    cookie_name: "hideOverlay",
+    cookie_expiry: "1", // 1 day
+    title: "",
+    subtitle: "",
+    paragraph: "",
+    button_label: "Donate Now",
+    image: "",
+    other_label: "$ other",
+    donation_form: "https://act.oceana.org/page/95375/donate/1",
+    trigger: "0", // int-seconds, px-scroll location, %-scroll location, exit-mouse leave
+  };
+  private scriptTag = document.querySelector(
+    "script[src*='oceana-en-overlay.js']"
+  );
+  constructor() {
+    if (!this.shouldRun()) {
+      console.log("Overlay Not Running");
+      return;
+    }
+
+    // Document Load
+    if (document.readyState !== "loading") {
+      this.run();
+    } else {
+      document.addEventListener("DOMContentLoaded", () => {
+        this.run();
+      });
+    }
+  }
+
+  private shouldRun() {
+    const hideOverlay = !!parseInt(crumbs.get(this.options.cookie_name)); // Get cookie
+    return !hideOverlay;
+  }
+
+  private run() {
+    this.loadOptions();
+    this.renderOverlay();
+    // Uncomment next line before publishing
+    // this.setCookie();
+  }
+  private renderOverlay() {
+    const markup = `
+            <div class="overlay-container">
+                <a href="#" class="button-close"></a>
+                <div class="overlay-content">
+                    <div class="overlay-logo">
+                    <img loading="lazy" alt="Oceana" src="https://oceana.org/wp-content/uploads/sites/18/2021/05/cropped-logo_en.png">
+                    </div>
+                    <div class="overlay-body">
+                        <h1 class="overlay-title">${this.options.title}</h1>
+                        <h2 class="overlay-subtitle">${
+                          this.options.subtitle
+                        }</h2>
+                        <hr>
+                        <p class="overlay-paragraph">${
+                          this.options.paragraph
+                        }</p>
+                        <div class="overlay-form">
+                            <form action="${
+                              this.options.donation_form
+                            }" method="GET">
+                              ${this.renderAmounts()}
+                              <button class="button button-primary">${
+                                this.options.button_label
+                              }</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `;
+    const overlay = document.createElement("div");
+    overlay.id = this.overlayID;
+    overlay.classList.add("is-hidden");
+    overlay.classList.add("oceana-en-overlay");
+    if (this.options.image) {
+      overlay.classList.add("has-image");
+      overlay.style.backgroundImage = `url(${this.options.image})`;
+    }
+    overlay.innerHTML = markup;
+    const closeButton = overlay.querySelector(
+      ".button-close"
+    ) as HTMLLinkElement;
+    closeButton.addEventListener("click", this.close.bind(this));
+    // I've commented out the following line because the overlay is fullscreen, so there's no "click outside to close" functionality
+
+    // overlay.addEventListener("click", (e: MouseEvent | KeyboardEvent) => {
+    //   if ((e.target as HTMLDivElement).id === this.overlayID) {
+    //     this.close(e);
+    //   }
+    // });
+    document.addEventListener("keyup", (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        closeButton.click();
+      }
+    });
+    const otherAmount = overlay.querySelector(
+      "input[name='transaction.donationAmt'].form-text"
+    );
+    if (otherAmount) {
+      otherAmount.addEventListener("focus", () => {
+        // Uncheck all other amounts
+        const inputs = overlay.querySelectorAll(
+          "input[type='radio']"
+        ) as NodeListOf<HTMLInputElement>;
+        for (let i = 0; i < inputs.length; i++) {
+          const input = inputs[i];
+          input.checked = false;
+        }
+      });
+    }
+
+    this.overlay = overlay;
+    document.body.appendChild(overlay);
+    const triggerType = this.getTriggerType(this.options.trigger);
+    if (triggerType === false) {
+      this.options.trigger = "2000";
+    }
+    if (triggerType === "seconds") {
+      this.options.trigger = (Number(this.options.trigger) * 1000).toString();
+    }
+    if (triggerType === "seconds" || triggerType === false) {
+      window.setTimeout(this.open.bind(this), Number(this.options.trigger));
+    }
+    if (triggerType === "exit") {
+      document.body.addEventListener("mouseout", (e) => {
+        if (e.clientY < 0 && !this.triggered) {
+          this.open();
+          this.triggered = true;
+        }
+      });
+    }
+    if (triggerType === "pixels") {
+      document.addEventListener(
+        "scroll",
+        this.scrollTriggerPx.bind(this),
+        true
+      );
+    }
+    if (triggerType === "percent") {
+      document.addEventListener(
+        "scroll",
+        this.scrollTriggerPercent.bind(this),
+        true
+      );
+    }
+  }
+  private renderAmounts() {
+    let amounts = `<div class="overlay-amounts">`;
+    amounts += this.amounts
+      .map((amount) => {
+        return `
+      <div class="form-item form-type-radio form-item-amount">
+        <input type="radio" id="edit-amount-${amount}" name="transaction.donationAmt" value="${amount}">  
+        <label class="option" for="edit-amount-${amount}">$${amount}</label>
+      </div>
+      `;
+      })
+      .join("");
+    if (this.options.other_label) {
+      amounts += `
+        <div class="form-item form-type-textfield form-item-other">
+          <input placeholder="${this.options.other_label}" type="text" name="transaction.donationAmt" value="" class="form-text">
+        </div>
+        `;
+    }
+    amounts += `</div>`;
+    return amounts;
+  }
+
+  private getTriggerType(trigger: string) {
+    /**
+     * Any integer (e.g., 5) -> Number of seconds to wait before triggering the lightbox
+     * Any pixel (e.g.: 100px) -> Number of pixels to scroll before trigger the lightbox
+     * Any percentage (e.g., 30%) -> Percentage of the height of the page to scroll before triggering the lightbox
+     * The word exit -> Triggers the lightbox when the mouse leaves the DOM area (exit intent).
+     * With 0 as default, the lightbox will trigger as soon as the page finishes loading.
+     */
+
+    if (!isNaN(Number(trigger))) {
+      return "seconds";
+    } else if (trigger.includes("px")) {
+      return "pixels";
+    } else if (trigger.includes("%")) {
+      return "percent";
+    } else if (trigger.includes("exit")) {
+      return "exit";
+    } else {
+      return false;
+    }
+  }
+  private scrollTriggerPx() {
+    const triggerValue = Number(this.options.trigger.replace("px", ""));
+    if (window.scrollY >= triggerValue && !this.triggered) {
+      this.open();
+      this.triggered = true;
+    }
+  }
+  private scrollTriggerPercent() {
+    const triggerValue = Number(this.options.trigger.replace("%", ""));
+    const clientHeight = document.documentElement.clientHeight;
+    const scrollHeight = document.documentElement.scrollHeight - clientHeight;
+    const target = (triggerValue / 100) * scrollHeight;
+    if (window.scrollY >= target && !this.triggered) {
+      this.open();
+      this.triggered = true;
+    }
+  }
+  private setCookie() {
+    crumbs.set(this.options.cookie_name, 0, {
+      type: "day",
+      value: this.options.cookie_expiry,
+    });
+  }
+  private loadOptions() {
+    this.amounts = this.getScriptData("amounts", "35,75,100,250,500")
+      .split(",")
+      .map((amount) => parseInt(amount));
+    // Load options from script tag
+    for (const key in this.options) {
+      if (key in this.options) {
+        this.options[key] = this.getScriptData(key, this.options[key]);
+      }
+    }
+  }
+  private getScriptData(attribute: string, defaultValue = "") {
+    if (this.scriptTag) {
+      const data = this.scriptTag.getAttribute("data-" + attribute);
+      return data ?? defaultValue;
+    }
+    return defaultValue;
+  }
+  private open() {
+    this.overlay.classList.remove("is-hidden");
+    document.body.classList.add("has-modal");
+  }
+  private close(e: MouseEvent | KeyboardEvent) {
+    e.preventDefault();
+    crumbs.set(this.options.cookie_name, 1, {
+      type: "day",
+      value: this.options.cookie_expiry,
+    }); // Create a cookie
+    this.overlay.classList.add("is-hidden");
+    document.body.classList.remove("has-modal");
+  }
+}
